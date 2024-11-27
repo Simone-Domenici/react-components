bed69082942c2b517b637812075a989d231e499a
Simone-Domenici <domenici.simone00@gmail.com>
2024-11-27 16:44:46 +0100

    ADD post component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import Header from './assets/components/Header.jsx';
-import styles from './App.css';
+import Header from './components/Header.jsx';
+import Post from './components/Post.jsx'
+import styles from './App.module.css';
 
 
 const App = () => {
   return (
+    <div className={styles.app}>
       <Header />
+      <main className={styles.main}>
+        <Post
+          title="Titolo del Post"
+          text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos dolor fugit deserunt recusandae nemo. Quis, quia! Cupiditate, quidem dolor, inventore repellat in amet voluptas iusto nostrum ipsam vero adipisci dignissimos, temporibus sit iste impedit ducimus."
+          image="https://via.placeholder.com/600x400" 
+
+        />
+      </main>
+    </div>
   );
 };
 
