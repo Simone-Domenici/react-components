1088ea3a466d32c3b037779f88657052e05550eb
Simone-Domenici <domenici.simone00@gmail.com>
2024-11-27 15:47:03 +0100

    ADD header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,12 @@
+import React from 'react';
+import Header from './assets/components/Header.jsx';
+import styles from './App.css';
 
+
+const App = () => {
+  return (
+      <Header />
+  );
+};
+
+export default App;
